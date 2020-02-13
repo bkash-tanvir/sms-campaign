83aff89f0f739b95f07b4bda4e9f838d6fc8d65c
bkash-tanvir <fm.hossain@bkash.com>
2020-02-14 02:08:55 +0600

    Initialized Project

---
+++ src/main/java/com/adn/digital/smscampaign/SmsCampaignApplication.java
@@ -0,0 +1,13 @@
+package com.adn.digital.smscampaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsCampaignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsCampaignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adn/digital/smscampaign/SmsCampaignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adn.digital.smscampaign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsCampaignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
